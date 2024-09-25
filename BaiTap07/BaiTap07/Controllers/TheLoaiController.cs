87d77a638cf3fc05e8f34e973bc1405a6630176d
huythai19 <tanoanh6576@gmail.com>
2024-09-25 08:20:43 +0700

    BaiTap07

---
+++ BaiTap07/BaiTap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,98 @@
+﻿using BaiTap07.Data;
+using BaiTap07.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiTap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //var theloai = _db.TheLoai.ToList();
+            var theloai = _db.TheLoai.Where(t => t.Id > 3 || t.DateCreated < new DateTime(2022, 2, 22)).ToList();
+            ViewBag.theloai = theloai;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoai theloai)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TheLoai.Add(theloai);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+
+            return View(theloai);
+        }
+        [HttpPost]
+        public IActionResult Edit(TheLoai theloai)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TheLoai.Update(theloai);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+
+            return View(theloai);
+        }
+        [HttpGet]
+        public IActionResult Details (int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.TheLoai.Find(id);
+
+            return View(theloai);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var theloai = _db.TheLoai.Find(id);
+            if (theloai == null)
+            {
+                return NotFound();
+            }
+            _db.TheLoai.Remove(theloai);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
